29cfc9b629aa62bc55c587d90c8b77e3e18ab3e7
Chris Taylor <chris11.taylor@nttdata.com>
2024-05-14 11:25:12 -0500

    Debug logging

--- src/versioning/entrypoint.py
+++ src/versioning/entrypoint.py
@@ -1,9 +1,11 @@
 from semver import Version
 try:
-    from __version__ import version as VERSION
+    from .__version__ import version as VERSION
+    print("Used __version__ to determine version")
 except ImportError:
     from setuptools_scm import get_version
     VERSION = get_version(version_scheme="only-version", local_scheme="no-local-version")
+    print("Used setuptools_scm to determine version")
 
 SEMANTIC_VERSION = Version.parse(VERSION)
 
